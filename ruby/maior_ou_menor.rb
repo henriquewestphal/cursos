b256eae7c3b35425fb9a17fc2fc4b513d7617241
Henrique Westphal <henriquewlaves@gmail.com>
2016-09-29 21:01:53 -0300

    ruby2

--- ruby/maior_ou_menor.rb
+++ ruby/maior_ou_menor.rb
@@ -1,43 +1,50 @@
-puts  "Jogo da adivinhação!\n\n"
-
-puts "qual é o seu nome?"
-$nome = gets
-
-puts "muito prazer, " + $nome
-puts "\nvamos começar o jogo\n\n\n"
-puts "escolhendo um numero secreto  de 0 a 200"
-$numero_escolhido = 20
-puts "escolhido\n\n\n"
-
-$numero_tentativas = 3
-for $tentativas in 1..$numero_tentativas
-	
-	puts "tentariva:  " + $tentativas.to_s + " de  " + $numero_tentativas.to_s
-
-	puts "\n\n\n\ntente adivinhar o numero escolhido?"
-	$chute = gets
-
-
-	puts "\nvoce chutou, " + $chute
-
-	$acertou = $numero_escolhido.to_i == $chute.to_i
-
-	if $acertou
-	 	puts "Parabens!! Voce acertou"
-	 	break
-	 else
-	 	puts "Infelizmente voce errou"
-
-	 	if $numero_escolhido.to_i > $chute.to_i
-	 		puts "\n\no numeor segreto é maior\n\n"
-	 	else
-	 		puts  "\n\no numero segreto é menor\n\n"
-
-	 	end 
-
-	 end 
+def boas_vindas
+	puts  "Jogo da adivinhação!\n\n"
+	puts "qual é o seu nome?"
+	nome = gets
+	puts "muito prazer, " + nome
+	puts "\nvamos começar o jogo\n\n\n"
 end
 
+def escolhendo_numero_secreto
+	puts "escolhendo um numero secreto  de 0 a 200"
+	numero_escolhido = 20
+	puts "escolhido\n\n\n"	
+	numero_escolhido
+end
 
+def pede_um_numero (tentativas, numero_tentativas)
 
+	puts "tentariva:  " + tentativas.to_s + " de  " + numero_tentativas.to_s
+	puts "\n\n\n\ntente adivinhar o numero escolhido?"
+	chute = gets
+	puts "\nvoce chutou, " + chute
+	chute.to_i
+end
 
+def verifica_se_acertou (numero_secreto, chute)
+	acertou = numero_secreto == chute
+
+	if acertou
+	 	puts "Parabens!! Voce acertou"
+	 	return true
+	end
+
+	puts "Infelizmente voce errou"
+	if numero_secreto > chute
+	 	puts "\n\no numeor segreto é maior\n\n"
+	else
+		puts  "\n\no numero segreto é menor\n\n"
+	end  
+	false
+end
+
+boas_vindas
+numero_secreto = escolhendo_numero_secreto
+
+numero_tentativas = 3
+for tentativas in 1..numero_tentativas
+	
+	chute = pede_um_numero tentativas, numero_tentativas
+	break if verifica_se_acertou numero_secreto, chute
+end
