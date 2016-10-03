f91efdf6dd4c6e7a9ac2705aabc19cab5832e162
root <root@localhost.localdomain>
2016-10-03 09:19:21 -0300

    nivel de dificudade

--- ruby/maior_ou_menor.rb
+++ ruby/maior_ou_menor.rb
@@ -1,14 +1,30 @@
 def boas_vindas
-	puts  "\n\n         Benvindo, \n   Jogo da adivinhação!\n\n"
+	puts "@@@@@@@@@@@@@@@@@@@@@"
+	puts "@@@    Benvindo!! @@@"
+	puts "@@@@@@@@@@@@@@@@@@@@@"
+	puts "Jogo da adivinhação!\n\n"
 	puts "qual é o seu nome?"
-	nome = gets.strip
-	puts "muito prazer, #{nome}"
-	puts "\nvamos começar o jogo\n\n\n"
+	nome = gets.strip.upcase
+	puts "Muito prazer, #{nome}"
+	puts "\nvamos começar o jogo!!\n\n\n"
+	nome
 end
 
-def escolhendo_numero_secreto
-	puts "escolhendo um numero secreto  de 0 a 200"
-	numero_escolhido = 20
+def escolhendo_numero_secreto (dificudade)
+	case dificudade
+	when 1 
+		maximo = 30
+	when 2
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	else
+		maximo = 200
+	end	
+	puts "escolhendo um numero secreto  de 0 a #{maximo}"
+	numero_escolhido = rand(maximo)
 	puts "escolhido\n\n\n"	
 	numero_escolhido
 end
@@ -39,17 +55,54 @@ def verifica_se_acertou (numero_secreto, chute)
 	false
 end
 
-boas_vindas
-numero_secreto = escolhendo_numero_secreto
-
-numero_tentativas = 5
-chutes = []
-
-for tentativas in 1..numero_tentativas
-	
-	chute = pede_um_numero chutes, tentativas, numero_tentativas
-	chutes << chute.to_i
-
-	break if verifica_se_acertou numero_secreto, chute
+def pede_dificudade
+	puts "Escolha o nivel de dificudade"
+	puts "sendo: (1)-muito facil, (2)-facil, (3)-medio, (4)-dificil, (5)-muito dificil"
+	dificudade = gets.to_i
 end
 
+def joga (nome, dificudade)
+
+	numero_secreto = escolhendo_numero_secreto dificudade
+	pontos = 1000
+	numero_tentativas = 5
+	chutes = []
+
+	for tentativas in 1..numero_tentativas
+		
+		chute = pede_um_numero chutes, tentativas, numero_tentativas
+		chutes << chute
+
+		if nome == "henrique"
+			puts "Parabens!! Voce acertou"
+			break
+		end
+
+		perde_pontos = (chute - numero_secreto).abs / 2.0
+		pontos -= perde_pontos
+
+		break if verifica_se_acertou numero_secreto, chute
+	end
+	puts "você ganhou: #{pontos} pontos."
+
+	if verifica_se_acertou = false
+		puts "o numero secreto é #{numero_secreto}"
+	end
+	
+end
+
+def nao_quero_jogar?
+	puts "Deseja jogar novamente (S/N)"
+	nao_quero_jogar = gets.strip
+	nao_quero_jogar.upcase  == "N"	
+end
+
+nome = boas_vindas
+loop do
+	dificudade = pede_dificudade 
+	joga nome, dificudade
+	if nao_quero_jogar?
+		break
+	end
+	
+end
--- ruby/teste.rb
+++ ruby/teste.rb
@@ -1,15 +1,43 @@
+# adiciona valores usando um contador
+# e apontando qual o lugar onde deve se atribuir o valor desejado 
 
 chutes = []
-chute = 50
-chutes << chute.to_i
+tentativa_chute = 0
 
-chute = 150
-chutes << chute.to_i
+chute =50
+chutes[tentativa_chute] = chute.to_i
+tentativa_chute +=1
+
+chute =150
+chutes[tentativa_chute] = chute.to_i
+tentativa_chute +=1
 
 chute =300
-chutes << chute.to_i
+chutes[tentativa_chute] = chute.to_i
+tentativa_chute +=1
 
 puts chutes
+puts "\n\n\n\n"
 
 
+#outra forma de adicionar valor na array
+# sem usar contador
+#detretando o valor de chute e adicionando esse chute para o array
 
+chutes = []
+chute = 100
+chutes << chute
+chute = 300
+chutes << chute
+chute = 450
+chutes << chute
+chute = 600
+chutes << chute
+chute = 12344
+chutes << chute
+puts chutes.size
+
+puts "\n\n\n"
+for chute in chutes
+	puts chute
+end
