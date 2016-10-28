667d302e3cf6ce21940acdc7467ea56469726465
root <henriquewalves@gmail.com>
2016-10-28 01:00:11 -0200

    implementação de aletas

--- ruby/lojavirtual/app/controllers/produtos_controller.rb
+++ ruby/lojavirtual/app/controllers/produtos_controller.rb
@@ -2,10 +2,31 @@ class ProdutosController < ApplicationController
   def index
     @produtos_por_nome = Produto.order :nome
   end
+  def new
+    @produto = Produto.new
+
+  end
+
 
 
   def create
     valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
-    produto = Produto.create valores
+    @produto = Produto.new valores
+    if @produto.save
+      flash[:notice] = "produto salvo com sucesso"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+  def busca
+    @nome_a_buscar = params[:nome]
+    @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+  end
+
+  def destroy
+    id = params[:id]
+    Produto.destroy id
+    redirect_to root_url
   end
 end
